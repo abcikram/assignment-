82db1c001cf0937e57854ba57892cbc19a4cedd4
abcikram <110589147+abcikram@users.noreply.github.com>
2023-04-20 10:35:11 +0530

    Completed

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv"
 import "./db/conn.js";
 import router from "./routes/router.js";
-import path from path
+
 
 
 dotenv.config()
@@ -16,13 +16,6 @@ app.use(cors());
 app.use(router)
 
 
-//static path 
-
-app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-app.get('*', function(req,res){
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-})
 
 const port = 9002;
 
